2192e066c9d6cf490ae963ffa8dd423ae885dcc3
fer5899 <fdometallic@gmail.com>
2022-09-30 12:09:57 +0200

    Revert "Add basic style to the page"
    This reverts commit 857772ce79cf1ba1aec9fc90dbbf8846d50db7bf.

--- eventListeners.js
+++ eventListeners.js
@@ -1,5 +1,5 @@
 const buttons = document.querySelectorAll("button");
 
 buttons.forEach(button => button.addEventListener("click", 
-    e => playRound(e.target.id, getComputerChoice())
+    e => playRound(e.target.classList[0], getComputerChoice())
     ));
